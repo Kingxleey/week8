373e06b13698dc98548f5bf76fa26a598b6e9318
Okoli <kingxleey@gmail.com>
2022-08-24 18:40:37 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require ("express")
+const PORT = 2000;
+const user = require ("./USer.js");
+const app = express ();
+const mongoose = require ("mongoose");
+const Database = "mongodb+srv://admin:admin@cluster0.fflhve9.mongodb.net/TestTD?retryWrites=true&w=majority"
+app.use(express.json());
+app.post('/signup', async(req, res) =>
+{
+    const reqBody = req.body;
+    console.log(reqBody, req.body);
+    const user = new User (reqBody);
+    console.log (user);
+    await user.save();
+    return res.status(200).send(user);
+})
+app.put ('/update', async (req, res) =>
+{
+const findUser = await user. findById(req.body.id);
+findUser.name = req.body.name;
+findUser.email = req.body.email;
+findUser.password = req.body.password;
+await findUser.save ();
+
+return res.status(201).send(findUser);
+})
+app.post("/login", async(req,res) =>
+{
+const {email, password} = req.body;
+const user = await user.findOne ({email, password});
+if (!user) return res.status (404).send("user not found");
+return res.status (200).send(user);
+})
+
+const startServer = async() =>
+{
+try {
+    mongoose.connect(Database)
+    console.log ("connected to the database")
+    
+app.listen(PORT,() => 
+{
+    console.log (`server is running on port ${PORT}`);
+})
+}
+
+
+   catch(error) {
+    console.log("error")
+   }
+}
+
+startServer();
+
+
